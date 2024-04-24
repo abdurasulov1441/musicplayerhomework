f68cfda856934cff0c06261fa09da0b9c49d446f
Abdulaziz <72754917+abdurasulov1441@users.noreply.github.com>
2024-04-24 22:38:44 +0500

    hello

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,125 +1,224 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color.fromARGB(255, 34, 21, 52), 
+            Color.fromARGB(255, 71, 34, 13),
+            ])
+          ),
+
+          child: Column(children: [
+          
+          
+          MenuBasic(),
+          SearchCont(),
+          ListMusic(),
+          ListentMusic(),
+
+          ],)
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MenuBasic extends StatelessWidget {
+  const MenuBasic({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return SafeArea(child: Container(
+      child: Column(children: [
+        Text('Home',style: TextStyle(fontSize: 20,color: Colors.white),),
+        SizedBox(height: 5,),
+        Container(
+          width: 220,
+          height: 30,
+          color: Color.fromARGB(255, 18, 20, 46),
+          padding: EdgeInsets.only(left: 5,right: 5),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            Text('Album',style: TextStyle(color: Colors.white,fontSize: 15),),
+            Text('Playlist',style: TextStyle(color: Colors.white,fontSize: 15),),
+            Text('Settings',style: TextStyle(color: Colors.white,fontSize: 15),),
+   
+            
+            ],),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],),
+    ));
+  }
+}
+
+
+class SearchCont extends StatelessWidget {
+  const SearchCont({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          width: double.infinity,
+          height: 30,
+          color: Color.fromARGB(255, 18, 20, 46),
+          margin: EdgeInsets.only(left: 20,right: 20,top: 30),
+          child: Row(children: [
+SizedBox(width: 10,),
+            Icon(Icons.search,color: Color.fromARGB(100, 255, 255, 255),),
+            SizedBox(width: 10,),
+            Text('search',style: TextStyle(color: const Color.fromARGB(100, 255, 255, 255),fontSize: 15))
+
+          ],),
+
+
     );
+  }
+}
+
+
+class ListMusic extends StatelessWidget {
+  const ListMusic({super.key});
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(children: [
+        SizedBox(width: 20,height: 100,),
+     Text('My songs',style: TextStyle(color: Colors.white,fontSize: 30),),
+      
+      ],)
+      
+ 
+      );
+  }
+}
+
+
+class ListentMusic extends StatelessWidget {
+  const ListentMusic({super.key});
+
+static List <String> name = [
+  'DVRST',
+  'Eminem',
+  'Sahara',
+  'J.Cole',
+  'Kanye West',
+  'NGNX',
+  'Everest',
+  'Next Time'
+
+];
+
+
+static List <String> secondName = [
+  'Dream Space',
+  'Close eyes',
+  'Slan',
+  'Henson',
+  'Alan walker',
+  'Metamorphothis',
+  'Yellow claw',
+  'Burak Yeter',
+
+];
+
+static List <String> images = [
+'images/1.png',
+'images/2.jpg',
+'images/3.jpg',
+'images/1.png',
+'images/2.jpg',
+'images/3.jpg',
+'images/1.png',
+'images/2.jpg',
+'images/3.jpg',
+'images/1.png',
+'images/2.jpg',
+
+
+
+];
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: double.infinity,
+        child:
+      
+      ListView.separated(
+        
+        itemBuilder: (context,index){
+          return Container(
+            margin: EdgeInsets.only(left: 20,right: 20),
+            child: Row(children: [
+              
+                //Icon(Icons.media_bluetooth_off,size: 50,),
+                Card(child: Image.asset(images[index],width: 100,height: 100,),
+                
+                ),
+
+
+                SizedBox(width: 20,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                Text(name[index],style: TextStyle(color: Colors.white,fontSize: 20),),
+                Text(secondName[index],style: TextStyle(color: Colors.white,fontSize: 15),),
+                ],)
+
+              
+              // Column(children: [
+              //   Text(name[index],style: TextStyle(color: Colors.white),)
+              // ],)
+              ],),
+          );
+          
+          
+      
+        }, 
+        separatorBuilder: (context,index){
+          return SizedBox();
+          
+          
+          
+        }, 
+        itemCount: name.length)
+      
+      
+      ,),
+    );
+    
   }
 }
